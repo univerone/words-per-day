62b6bc9fe4b1bae17259953833c6d4c89a257b45
江姗姗 <1900022730@pku.edu.cn>
2020-08-13 12:32:22 +0800

    fix generate image related file path

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
-// 每日一句插件配置项（必填项）
-const FONT_DIR: string = './font' // 字体文件夹
-const IMAGE_DIR: string = './image' // 存放图片的文件夹
+import path from 'path';
+const FONT_DIR: string = path.join(__dirname.replace('dist/src',''), '/font'); // 字体文件夹
+const IMAGE_DIR: string = path.join(__dirname.replace('dist/src',''), '/image'); // 存放图片的文件夹
 
 export { FONT_DIR, IMAGE_DIR }
